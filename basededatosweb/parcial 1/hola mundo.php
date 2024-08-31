a0b5165c338d3f71ddeceb573cd37bccdf52e75b
VariniaCetis107 <159467408+VariniaCetis107@users.noreply.github.com>
2024-08-30 18:38:20 -0700

    Varinia Manjarrez 30/08/2024

---
+++ basededatosweb/parcial 1/hola mundo.php
@@ -0,0 +1,17 @@
+<?php
+
+  $nombre = "Varinia M";
+  $numero = 4;
+  $numero2 = "4";
+
+  $total = $numero + $numero2;
+
+  echo "total =" . $total;
+
+  echo "<h1>HOLA MUNDO</h1>";
+
+  for($i=0; $i<10; $i++){
+    echo $i . "<br>";
+  }
+
+?>
